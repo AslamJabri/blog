ff2ec0199056f283b7db40c2b0fdc84a3cea1455
AslamJabri <jabri.aslam@gmail.com>
2021-10-22 14:52:31 +1100

    Deply

--- app.js
+++ app.js
@@ -86,6 +86,15 @@ app.post("/contact", function(req, res){
 })
 
 //server on port
-app.listen(process.env.port, function() {
-  console.log("Server started on port 3000");
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+app.listen(port);
+
+
+
+app.listen(port, function() {
+  console.log(`Server started on port {3000}`);
 });
